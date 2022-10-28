6f88e15394b138a3d65b2a2b595d2e8678c233d0
Ma. Pia Paula Celemin <116002019+Celemin-Ma-Pia@users.noreply.github.com>
2022-10-28 12:28:39 +0800

    Create if-else-example.py

---
+++ if-else-example.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+if num >= 10:
+    if num <= 20:
+        print("This is between 10 and 20")
+    else:
+        print("This is over 20")
+else:
+    print("This is under 10")
